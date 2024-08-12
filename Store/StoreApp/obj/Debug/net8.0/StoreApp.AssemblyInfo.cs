c95101ef3070de8bb637041aa1dbe2228d6fc7df
Talha Sarı <117735845+TalhaSariii@users.noreply.github.com>
2024-08-12 17:46:07 +0300

    .

--- Store/StoreApp/obj/Debug/net8.0/StoreApp.AssemblyInfo.cs
+++ Store/StoreApp/obj/Debug/net8.0/StoreApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StoreApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d8c0431f7f31ea65d040c3c0be54403f6ff0d96")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32b6820a85eb1ee4c8459e05686c3aecf63ee3dd")]
 [assembly: System.Reflection.AssemblyProductAttribute("StoreApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StoreApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
