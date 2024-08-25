08d1de65c658cc922070b47ed5f9c6a44e00919d
Talha Sarı <117735845+TalhaSariii@users.noreply.github.com>
2024-08-25 19:36:48 +0300

    Repository manageradd

--- Store/StoreApp/Controllers/ProductController.cs
+++ Store/StoreApp/Controllers/ProductController.cs
@@ -2,6 +2,7 @@ using Entities.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Repositories;
+using Repositories.Contracts;
 
 
 
@@ -11,24 +12,25 @@ namespace StoreApp.Controllers
     public class ProductController : Controller
     {
 
-        private readonly RepositoryContext _context;
-        public ProductController(RepositoryContext context)
+    private readonly IRepositoryManager _manager;
+
+        public ProductController(IRepositoryManager manager)
         {
-            _context=context;
+            _manager = manager;
         }
-        
+
         public IActionResult Index()
         {
           
-            var model= _context.Products.ToList();
+            var model= _manager.Product.GetAllProducts(false);
             return View(model);
            
         }
 
         public IActionResult Get(int id)
         {
-            Product product= _context.Products.First(p => p.ProductId.Equals(id));
-            return View(product);
+          //  Product product= _context.Products.First(p => p.ProductId.Equals(id));
+            throw new NotImplementedException();
         }
     }
 }
--- Store/StoreApp/Program.cs
+++ Store/StoreApp/Program.cs
@@ -1,13 +1,17 @@
 using Microsoft.EntityFrameworkCore;
 using Repositories;
+using Repositories.Contracts;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<RepositoryContext>(options=>
 {
         options.UseSqlite(builder.Configuration.GetConnectionString("sqlconnection"),
-        b =>b.MigrationsAssembly("StoreApp"));
+        b =>b.MigrationsAssembly("StoreAppd"));
 });
+
+builder.Services.AddScoped<IRepositoryManager,RepositoryManager>();
+builder.Services.AddScoped<IProductRepository,ProductRepository>();
 
 var app = builder.Build();
 
