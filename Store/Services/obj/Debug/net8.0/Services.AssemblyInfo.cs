b425a7a83a79f9ff7befa016ad5050f2604e4fa7
Talha Sarı <117735845+TalhaSariii@users.noreply.github.com>
2024-10-29 20:21:10 +0300

    _Manage Orders

--- Store/Services/obj/Debug/net8.0/Services.AssemblyInfo.cs
+++ Store/Services/obj/Debug/net8.0/Services.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a484e74a0e0f8a9f9c63d0ada28a245f36d5275")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a7e659de5c2ae1d4b7eeca87e016823cf944de1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
