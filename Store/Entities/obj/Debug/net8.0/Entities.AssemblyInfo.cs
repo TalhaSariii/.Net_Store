fc57198d19fbea4bbc2ef1b9fcfe43f66892ee4a
Talha Sarı <117735845+TalhaSariii@users.noreply.github.com>
2024-11-10 21:09:41 +0300

    _Login

--- Store/Entities/obj/Debug/net8.0/Entities.AssemblyInfo.cs
+++ Store/Entities/obj/Debug/net8.0/Entities.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Entities")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd69d37e17e76e903fcc60e17563dca2af2b157e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+549673799cfa3e9a2711684cd1b2c55e7e60e54e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Entities")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Entities")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
