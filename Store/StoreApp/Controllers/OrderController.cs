425513df1df2b0342bf1d27cd0f45f36c96bea21
Talha Sarı <117735845+TalhaSariii@users.noreply.github.com>
2024-12-07 13:55:31 +0300

    Autohrizon

--- Store/StoreApp/Controllers/OrderController.cs
+++ Store/StoreApp/Controllers/OrderController.cs
@@ -1,4 +1,5 @@
 using Entities.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 using Microsoft.EntityFrameworkCore.Infrastructure;
@@ -17,7 +18,8 @@ namespace StoreApp.Controllers
             _manager = manager;
             _cart = cart;
         }
-
+        
+        [Authorize]
         public ViewResult Checkout() => View(new Order());
 
         [HttpPost]
